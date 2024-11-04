3741f602fb60e8e1074cab6492e1b473973907b0
TechU <87014776+Umeshgardas@users.noreply.github.com>
2024-11-04 12:44:44 +0530

    execution context

---
+++ Week2/Day2/batch 1/index.js
@@ -0,0 +1,57 @@
+// // console.log(y);
+// // console.log("value of Y" + " " + y);
+firstname();
+
+var x = undefined;
+// var x = 7;
+
+// var x = "Amit";
+// console.log("value of X" + " " + x);
+// {
+//   var x = true;
+//   let y = 11;
+//   y = true;
+//   y = false;
+//   console.log("value of Y" + " " + y);
+// }
+// let y = 32;
+// console.log("value of Y" + " " + y);
+
+// const PI = 3.14;
+// const color = "#fff";
+// {
+//   const PI = 3464;
+
+//   console.log("value of Y in blocl scope" + " " + PI);
+// }
+// console.log("value of Y in global scope" + " " + PI);
+function firstname() {
+  return "maxxzoom";
+}
+// var x= 3.99;
+// console.log(x);
+
+/* Assignement Operator */
+// let x = 6;
+// x |= 5; /* x = x | 5 */
+// 0101 = 1010
+
+// let x = 10;
+// let y = "10";
+// 10 === 10;
+// ==  it will check only values present in the variable
+// === it will check value and data type of that variable
+
+// document.getElementById("demo").innerHTML = x === y ? "Value and data type are same":"value and data type are different";
+document.getElementById("demo").innerHTML = 10 + 40 + "10" - 10;
+/* 5010 -10*/
+/* Datatypes  of JS */
+
+// /* String */  "Yellow"
+// /* Number */  19/1.9
+// /* Boolean */  true/false
+// /* Undefined */  undefined
+// /* Null */  Null
+// /* Symbol */
+// /* Object */  {"firstname","lastname"}
+// /* Bigint */ 298349827039834
---
+++ Week2/Day2/batch 2/index.js
@@ -0,0 +1,54 @@
+// var a = 10;
+// a = 20;
+
+// let b = 40;
+// b = 50;
+// {
+//   let b = 35;
+//   b = 60;
+//   var a = 30;
+//   console.log("value of b variable" + " " + b);
+// }
+// const PI = 3.14;
+// PI = 3.555;
+
+// console.log("value of b variable" + PI);
+// console.log(a);
+// console.log("value of b variable" + " " + b);
+
+// let x = 10;
+// var a = 40;
+// let y = 20;
+// console.log("value of y top of the scope" + " " + y);
+// console.log("value of a top of the scope" + " " + a);
+// {
+//   var a = 70;
+//   let y = 50;
+//   console.log("value of y in  the scope" + " " + y);
+//   console.log("value of a in the scope" + " " + a);
+// }
+// console.log("value of y bottom of the scope" + " " + y);
+// console.log("value of a bottom of the scope" + " " + a);
+// document.getElementById("demo").innerHTML = x * y;
+// document.getElementById("testing").innerHTML = x / y;
+
+/* Data type of Javascript 
+/* String */ /* ("firstname"); */
+/* Number */ /* 10, 20; */
+/* Bigint */ /* 4165196851651; */
+/* Boolean */ /* true / false; */
+/* Undefined */ /* undefined; */
+/* Null  null; */
+/* Symbol */
+/* Object */ /* {
+  "first", "second", 10, true, false
+} */
+
+// let a = 40;
+// let b = "40";
+
+// document.getElementById("demo").innerHTML = 14 > 13 ? "true" : "false";
+// /* */ 1100;
+// 1110;
+// 1101;
+// 1111;
