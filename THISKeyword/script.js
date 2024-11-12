3d7c2fba0190d8216cff6c875441de94104a9e9f
TechU <87014776+Umeshgardas@users.noreply.github.com>
2024-11-12 12:33:35 +0530

    call apply bind method

--- THISKeyword/script.js
+++ THISKeyword/script.js
@@ -8,16 +8,16 @@
 // window.addEventListener("online", () => console.log("Back online!"));
 // window.addEventListener("offline", () => console.log("Lost connection."));
 
-function myfun() {
-  console.log(this);
-}
-let data = () => {
-  console.log(this);
-};
+// function myfun() {
+//   console.log(this);
+// }
+// let data = () => {
+//   console.log(this);
+// };
 
-console.log(this);
+// console.log(this);
 
-myfun();
+// myfun();
 
 // This Keyword
 //  2 ways of functions
@@ -25,41 +25,42 @@ myfun();
 
 //  Arrow function -> this will never change whatever it is
 
-const student = {
-  id: 2,
-  firstname: "Anuj",
-  lastname: "shinde",
-  getFullName: function () {
-    return this.firstname + " " + this.lastname;
-  },
-  getGreetmessage: function (mess) {
-    return this.firstname + " " + "Welcome" + " " + mess;
-  },
-  // arrowFunction: () => {
-  //   return this.firstname + " " + "arrow" + " ";
-  // }
-};
+// const student = {
+//   id: 2,
+//   firstname: "Anuj",
+//   lastname: "shinde",
+//   getFullName: function () {
+//     return this.firstname + " " + this.lastname;
+//   },
+//   getGreetmessage: function (mess) {
+//     return this.firstname + " " + "Welcome" + " " + mess;
+//   },
+//   // arrowFunction: () => {
+//   //   return this.firstname + " " + "arrow" + " ";
+//   // }
+// };
 
-console.log(student.getFullName());
-console.log(student.arrowFunction());
+// console.log(student.getFullName());
+// console.log(student.arrowFunction());
 
-const student2 = {
-  firstname: "amit",
-  lastname: "sonare",
-  age: 25,
-  
-};
+// const student2 = {
+//   firstname: "amit",
+//   lastname: "sonare",
+//   age: 25,
 
-const arr=[20,38,4434];
+// };
+
+// const arr=[20,38,4434];
 // call method -> when you use call , you can pass the parameter normally
-console.log(student.getFullName.call(student2));
-console.log(student.getGreetmessage.call(student2, "How are you"));
-// console.log(student.arrowFunction.call(student2, "How are you",""));
+// console.log(student.getFullName.call(student2));
+// console.log(student.getGreetmessage.call(student2, "How are you"));
+// // console.log(student.arrowFunction.call(student2, "How are you",""));
 
-// apply method ->when you use apply , you have to pass params in array
-console.log(student.getGreetmessage.apply(student2, arr));
+// // apply method ->when you use apply , you have to pass params in array
+// console.log(student.getGreetmessage.apply(student2, arr));
 
-// bind -> it returns the new function with specified object
-let bindmethod = student.getGreetmessage.bind(student2, "");
-console.log(bindmethod());
+// // bind -> it returns the new function with specified object
+// let bindmethod = student.getGreetmessage.bind(student2, "");
+// console.log(bindmethod());
+
 
